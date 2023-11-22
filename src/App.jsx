359977e728498b3e791c87ce21894382e53ccfd2
NitinAsogekar <nitinasogekar@gmail.com>
2023-11-22 22:12:10 +0530

    d

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Contact from "./components/Contact";
 import About from "./components/About";
 import Layout from "./Layout";
 import { I18nextProvider, useTranslation } from "react-i18next";
-import i18n from "./i18n";
+import i18n from "../i18n";
 
 const router = createBrowserRouter([
   {
