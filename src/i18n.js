0dc5619d732167f44d115cd2dad3e209137b48b8
NitinAsogekar <nitinasogekar@gmail.com>
2023-11-29 21:35:27 +0530

    changes

--- src/i18n.js
+++ src/i18n.js
@@ -2,9 +2,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import HttpApi from "i18next-http-backend";
-import translationEN from "./locales/en/translation.json";
-
-console.log(translationEN);
 
 i18n
   .use(HttpApi)
@@ -16,7 +13,7 @@ i18n
       escapeValue: false,
     },
     backend: {
-      loadPath: "src/locales/{{lng}}/translation.json",
+      loadPath: "./src/{{lng}}/translation.json",
     },
   });
 
